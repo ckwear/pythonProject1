b823efa90bdac54bd748f31420857cee206b2834
tjkim <tjkim@vetec.co.kr>
2021-04-29 16:58:02 +0900

    4월 29일 목요일

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import selenium
+from selenium import webdriver
+from bs4 import BeautifulSoup
+URL = 'https://www.acmicpc.net/ranklist'
 
+driver = webdriver.Chrome(executable_path='./chromedriver')
+driver.get(url=URL)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+itme_cnt = driver.find_elements_by_xpath('/html/body/div[2]/div[2]/div[3]/div[3]/div/table/tbody/tr')
+print(len(itme_cnt))
+for i in range(100):
+    driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[3]/div[3]/div/table/tbody/tr[' + str(i+1) +']/td[2]/a').click()
+    req = driver.page_source
+    soup=BeautifulSoup(req, 'html.parser')
+    selector = soup.find(id="u-solved")
+    print(selector)
+    driver.back()
